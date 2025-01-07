9cc1b9428d313d815143920e1532e248e94a0be8
Jaza Soares <166966224+JazaaSoares@users.noreply.github.com>
2025-01-07 17:19:09 -0300

    Atualização carrossel de habilidades

--- script.js
+++ script.js
@@ -5,3 +5,30 @@ $(document).ready(function(){
     });
 });
 
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    grabCursor: true,
+    navigation:{
+        nextE1: ".swiper-button-next",
+        prevE1: ".swiper-button-prev"
+
+    },
+    pagination:{
+        el:".swiper-pagination",
+        clickable: true
+    },
+    breakpoints:{
+        640:{
+            slidesPerView:2,
+            spaceBetween:20
+        },
+        800:{
+            slidesPerView:4,
+            spaceBetween:30
+        },
+        1170:{
+            slidesPerView:5,
+            spaceBetween:50
+        }
+    }
+});
