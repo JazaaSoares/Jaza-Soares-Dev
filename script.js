1fa7142ecbb4cc254880d73b40e8f8459297088f
Jaza Soares <166966224+JazaaSoares@users.noreply.github.com>
2025-01-15 14:49:10 -0300

    Adicionei contatos

--- script.js
+++ script.js
@@ -3,6 +3,35 @@ $(document).ready(function(){
         $('#mobile_menu').toggleClass('active');
         $('#mobile_btn').find("i").toggleClass('fa-x');
     });
+
+    const sections = $('.section');
+    const navItems = $('.nav-item');
+
+    $(window).on('scroll', function(){
+        const header = $('header');
+        let activeSectionIndex = 0;
+        const scrollPosition = $(window).scrollTop() - header.outerHeigth;
+
+        if(scrollPosition<=0){
+            header.css('box-shadow', 'none');
+        }else{
+            header.css('box-shadow','5px 1px 5px rgba(0,0,0,0.1)')
+        }
+
+        sections.each(function(i){
+                const section = $(this);
+                const sectionTop = section.offset().top - 96;
+                const sectionBottom = sectionTop + section.outerHeigth;
+
+                if(scrollPosition >= sectionTop && scrollPosition < sectionBottom){
+                    activeSectionIndex= i;
+                    return false;
+                } 
+        })
+
+        navItems.removeClass('active');
+        $(navItems[activeSectionIndex]).addClass('active');
+    })
 });
 
 const swiper = new Swiper('.swiper', {
@@ -10,8 +39,8 @@ const swiper = new Swiper('.swiper', {
     grabCursor: true,
     navigation:{
         nextE1: ".swiper-button-next",
-        prevE1: ".swiper-button-prev",
-        clickable: true
+        prevE1: ".swiper-button-prev"
+        
     },
     pagination:{
         el:".swiper-pagination",
