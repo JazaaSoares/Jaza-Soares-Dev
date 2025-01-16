1dbbd766f6b753cd2f275886808036fdc652abf3
Jaza Soares <166966224+JazaaSoares@users.noreply.github.com>
2025-01-15 23:42:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,46 @@
 $(document).ready(function(){
+    
     $('#mobile_btn').on('click',function(){
         $('#mobile_menu').toggleClass('active');
         $('#mobile_btn').find("i").toggleClass('fa-x');
     });
 
     $('#curriculo-button').on('click', function() {
-        const pdfUrl = '/arquivos/curriculum.pdf'; // Substitua pelo caminho do PDF
-        const link = document.createElement('a'); // Cria um elemento <a>
-        link.href = pdfUrl; // Define o caminho do PDF
-        link.download = 'arquivo.pdf'; // Nome do arquivo a ser baixado
-        link.click(); // Simula o clique no link
+        const pdfUrl = 'arquivos/curriculum.pdf'; 
+        const link = document.createElement('a'); 
+        link.href = pdfUrl; 
+        link.download = 'arquivo.pdf'; 
+        link.click(); 
     });
 
-    const sections = $('.section'); // Certifique-se de que as seções têm a classe .section
+    const sections = $('.section'); 
     const navItems = $('.nav-item');
-
-
 
     $(window).on('scroll', function() {
         const header = $('header');
         let activeSectionIndex = 0;
-        const scrollPosition = $(window).scrollTop() - header.outerHeight(); // Corrigido outerHeigth para outerHeight
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
 
         console.log(scrollPosition);
 
-        // Adiciona ou remove a sombra do header
         if (scrollPosition <= 0) {
             header.css('box-shadow', 'none');
         } else {
             header.css('box-shadow', '5px 1px 5px rgba(0,0,0,0.1)');
         }
 
-        // Determina qual seção está ativa
         sections.each(function(i) {
             const section = $(this);
-            const sectionTop = section.offset().top - header.outerHeight(); // Corrigido outerHeigth para outerHeight
+            const sectionTop = section.offset().top - 92.8; 
             const sectionBottom = sectionTop + section.outerHeight();
 
             if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
                 activeSectionIndex = i;
-                return false; // Interrompe o loop ao encontrar a seção ativa
+                return false; 
             }
         });
 
-        // Atualiza a classe 'active' no menu
+       
         navItems.removeClass('active');
         $(navItems[activeSectionIndex]).addClass('active');
     });
@@ -91,7 +88,56 @@ $(document).ready(function(){
         distance: '20%'
     });
     
+    const mode = document.getElementById('mode-icon');
 
+    mode.addEventListener('click', () => {
+        
+        const body = document.getElementById('all');
+        const cta = document.getElementById('cta');
+        const navbars = document.getElementById('navbars');
+        const navI = document.getElementById('nav_lista');
+        const mobNavList = document.getElementById('mobile_nav_lista');
+        const projects = document.getElementById('list-projects');
+        const description = document.getElementById('description');
+        const darkWave = document.getElementById('dark-wave');
+        const ligthWave = document.getElementById('ligth-wave');
+        const mobileBtn = document.getElementById('mobile_btn');
+        
+        
+        if(mode.classList.contains('fa-moon')){
+            mode.classList.remove('fa-moon');
+            mode.classList.add('fa-sun');
+            
+
+            body.classList.add('dark');
+            cta.classList.add('dark');
+            navbars.classList.add('dark');
+            navI.classList.add('dark');
+            mobNavList.classList.add('dark');
+            projects.classList.add('dark');
+            description.classList.add('dark');
+            darkWave.classList.add('dark');
+            ligthWave.classList.add('dark');
+            mobileBtn.classList.add('dark');
+
+            return;
+        }
+
+        mode.classList.remove('fa-sun');
+        mode.classList.add('fa-moon');
+
+        body.classList.remove('dark');
+        cta.classList.remove('dark');
+        navbars.classList.remove('dark');
+        navI.classList.remove('dark');
+        mobNavList.classList.remove('dark');
+        projects.classList.remove('dark');
+        description.classList.remove('dark');
+        darkWave.classList.remove('dark');
+        ligthWave.classList.remove('dark');
+        mobileBtn.classList.remove('dark');
+
+    });
 
 
 
